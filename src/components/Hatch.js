65e9d75c41f8801af2ecbec907ccc71b59ebc43a
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-11-26 00:55:33 +0000

    Door opens with state

--- src/components/Hatch.js
+++ src/components/Hatch.js
@@ -1,29 +1,39 @@
+import { useState, useEffect } from "react";
 import { createCalendar } from "../helper";
 import "./Hatch.css";
 
 export default function Hatch({ date }) {
   const dateNum = date.slice(0, 2);
+  const month = date.slice(3, 5);
+
+  const [doors, setDoors] = useState([]);
+  const [calendar, setCalendar] = useState(createCalendar);
 
   function handleClick(e) {
-    if (e.target.className === dateNum) {
-      e.target.style.backgroundColor = "red";
-    }
+    const newArray = calendar.map((door) => {
+      if (door.nr === 25) {
+        return { ...door, open: true };
+      }
+      return door;
+    });
+    setCalendar(newArray);
   }
+  console.log(calendar);
   return (
     <div className="board">
-      {createCalendar.map((num, index) => {
+      {calendar.map((obj, index) => {
         return (
-          <div className="hatch-container" onClick={handleClick}>
-            <p className={`${num.nr}, front-p-tag`}>{num.nr}</p>
+          <div className="hatch-container" onClick={handleClick} key={index}>
+            <h3 className={`${obj.nr}, front-h3-tag`}>{obj.nr}</h3>
             <img
-              className={`${num.nr}, front-img-tag`}
+              className={`${obj.nr}, front-img-tag`}
               width={120}
-              src={num.img}
+              src={obj.img}
               alt=""
             />
             <img
-              className={`${num.nr}, back-img-tag`}
-              src={num.backFile}
+              className={obj.open ? "opened" : `${obj.nr}, back-img-tag`}
+              src={obj.backFile}
               alt=""
             />
           </div>
@@ -32,3 +42,5 @@ export default function Hatch({ date }) {
     </div>
   );
 }
+
+// Number(dateNum)
